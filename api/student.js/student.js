633240ebf2ecee632224a7709dbc916496c435f8
Sharan <sharan@Sharans-MacBook-Air.local>
2026-03-05 18:56:46 +0530

    fix api folder

--- api/student.js/student.js
+++ api/student.js/student.js
@@ -1,11 +1,9 @@
 export default function handler(req, res) {
-  const student = {
+  res.status(200).json({
     name: "Rahul Sharma",
     progress: "75%",
     focusLevel: "Good",
     completedLessons: 12,
     pendingLessons: 3
-  };
-
-  res.status(200).json(student);
+  });
 }
